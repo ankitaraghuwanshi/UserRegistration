68b852c86a7d9934cdde95e738b35da1a50f1114
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-31 13:06:09 +0530

    [Ankita] Add: Program for registration of firstname

---
+++ Regex/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UserRegistration
+{
+    class UserRegistration
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First name");
+            string Firstname = Console.ReadLine();
+            UserRegexTest userRegexTest = new UserRegexTest();
+            string FirstName = Firstname;
+            userRegexTest.validFirstName(FirstName);
+
+        }
+    }
+}
---
+++ Regex/Regex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class UserRegexTest
+    {
+        public Regex FirstNameRegex = new Regex(@"^[A-Z][A-Za-z]{2,}$");
+
+        public void validFirstName(string FirstName)
+        {
+            Console.WriteLine("\nFirst Name:"+ FirstName);
+            if (FirstNameRegex.IsMatch(FirstName))
+            {
+                Console.WriteLine("First name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First name is invalid");
+            }
+        }
+    }
+}
